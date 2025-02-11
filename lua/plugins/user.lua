8eec1ebd8da5a12643fcb0e2c837c80dee9f1b62
AbiXnash <abixnash@gmail.com>
2025-02-11 21:16:52 +0530

    Plugin Installation Scratchpad, Git, Telescope Git Branch, Fugit2, Telekasten

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -12,7 +12,33 @@ return {
 		event = "BufRead",
 		config = function() require("lsp_signature").setup() end,
 	},
-
+	{
+		"mrloop/telescope-git-branch.nvim",
+	},
+	{
+		"SuperBo/fugit2.nvim",
+		build = false,
+		opts = {
+			width = 100,
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"nvim-lua/plenary.nvim",
+			{
+				"chrisgrieser/nvim-tinygit", -- optional: for Github PR view
+				dependencies = { "stevearc/dressing.nvim" },
+			},
+		},
+		cmd = { "Fugit2", "Fugit2Diff", "Fugit2Graph" },
+		keys = {
+			{ "<leader>F", mode = "n", "<cmd>Fugit2<cr>" },
+		},
+	},
+	{
+		"renerocksai/telekasten.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+	},
 	-- == Examples of Overriding Plugins ==
 	{ "folke/neoconf.nvim" },
 	{
