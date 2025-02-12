87977aa16789fea76bb932e571880260eaade8b2
AbiXnash <abixnash@gmail.com>
2025-02-12 12:31:29 +0530

    Autocomplete insert key changed to TAB

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,42 +1,45 @@
 --- @type LazySpec
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    local cmp = require "cmp"
-    local luasnip = require "luasnip"
-    local border_opts = {
-      border = "single",
-      winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-    }
+	"hrsh7th/nvim-cmp",
+	opts = function(_, opts)
+		local cmp = require "cmp"
+		local luasnip = require "luasnip"
+		local border_opts = {
+			border = "single",
+			winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+		}
 
-    return require("astrocore").extend_tbl(opts, {
-      completion = {
-        completeopt = "menu,menuone,noinsert",
-      },
-      window = {
-        completion = cmp.config.window.bordered(border_opts),
-        documentation = cmp.config.window.bordered(border_opts),
-      },
-      mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      },
-    })
-  end,
+		return require("astrocore").extend_tbl(opts, {
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+			window = {
+				completion = cmp.config.window.bordered(border_opts),
+				documentation = cmp.config.window.bordered(border_opts),
+			},
+
+			mapping = {
+				["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+				["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+				["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+				["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.confirm { select = true } -- Accept the suggestion
+					elseif luasnip.jumpable(1) then
+						luasnip.jump(1) -- Jump inside snippet
+					else
+						fallback() -- Default behavior
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if luasnip.jumpable(-1) then
+						luasnip.jump(-1) -- Jump back in snippet
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			},
+		})
+	end,
 }
