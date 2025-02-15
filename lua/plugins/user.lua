b8d3bdb6280f3ed603632a7e176f6b4d5a70840b
AbiXnash <abixnash@gmail.com>
2025-02-15 23:23:24 +0530

    Added fzf plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -96,7 +96,7 @@ return {
 		build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
 		dependencies = {
 			"nvim-telescope/telescope.nvim",
-			-- "ibhagwan/fzf-lua",
+			"ibhagwan/fzf-lua",
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
 		},
