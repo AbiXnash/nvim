94e7a0cc28af2f9fd176e10c4cb96e7a0cbed507
AbiXnash <abixnash@gmail.com>
2025-03-03 22:53:57 +0530

    Package Update

--- lua/community.lua
+++ lua/community.lua
@@ -4,20 +4,19 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python" },
-  -- heirline
-  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
-  -- codesnap
-  { import = "astrocommunity.media.codesnap-nvim" },
-  {
-    "mistricky/codesnap.nvim",
-    build = "make",
-    opts = {
-      save_path = "~/Pictures/screenshots/codesnap/",
-      code_font_family = "JetBrainsMono Nerd Font",
-      bg_color = "#a8b0d5",
-    },
-  },
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.recipes.heirline-vscode-winbar" },
+	{ import = "astrocommunity.media.codesnap-nvim" },
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		opts = {
+			save_path = "~/Pictures/screenshots/codesnap/",
+			code_font_family = "JetBrainsMono Nerd Font",
+			bg_color = "#a8b0d5",
+		},
+	},
+	{ import = "astrocommunity.motion.nvim-surround" },
 }
