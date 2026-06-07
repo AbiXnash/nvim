9f884d2700accaefc2a00e396f07139f89ad851a
Abinash <abinash@theabx.in>
2026-06-08 01:13:19 +0530

    chore(plugins): add mini.pairs for auto pairing (lightweight, fits mini ecosystem); theme cleanup already committed

--- lua/abx/plugins/tools/surround.lua
+++ lua/abx/plugins/tools/surround.lua
@@ -1,7 +1,14 @@
 return {
-    "echasnovski/mini.surround",
-    version = "*",
-    opts = {
-        -- Use default mappings (ys, ds, cs, etc.)
+    {
+        "echasnovski/mini.surround",
+        version = "*",
+        opts = {
+            -- Use default mappings (ys, ds, cs, etc.)
+        },
+    },
+    {
+        "echasnovski/mini.pairs",
+        version = "*",
+        opts = {},
     },
 }
